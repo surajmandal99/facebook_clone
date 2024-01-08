58c9e12b2fd5d9ec061b0563de622f391c5d0109
Suraj Mandal <20121A0566@vidyanikethan.asia>
2024-01-08 20:21:53 +0530

    initial_commit

---
+++ lib/features/posts/presentation/widgets/post_image_video_view.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+import '/features/posts/presentation/widgets/network_video_view.dart';
+
+class PostImageVideoView extends StatelessWidget {
+  const PostImageVideoView({
+    Key? key,
+    required this.fileType,
+    required this.fileUrl,
+  }) : super(key: key);
+
+  final String fileType;
+  final String fileUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    if (fileType == 'image') {
+      return Image.network(fileUrl);
+    } else {
+      return NetworkVideoView(
+        videoUrl: fileUrl,
+      );
+    }
+  }
+}
